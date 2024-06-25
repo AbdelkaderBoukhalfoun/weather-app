bbe5cc62b3e65394517e013eb18bdfafd9d96348
AbdelkaderBoukhalfoun <abdelkaderboukhalfoun@gmail.com>
2024-06-25 02:35:05 +0100

    Add config sample and update gitignore

--- config.js
+++
@@ -1,5 +0,0 @@
-const CONFIG = {
-    API_KEY: 'e4331e686bba704d58f43feadc4265fc'
-};
-
-export default CONFIG;
---
+++ config.sample.js
@@ -0,0 +1,6 @@
+// config.sample.js
+const CONFIG = {
+    API_KEY: 'YOUR_API_KEY_HERE'
+};
+
+export default CONFIG;
